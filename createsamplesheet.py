bcdc9ab9d8465cb5f8e85547e5dde1adf00110ca
Hatim Bouzian <bouzia00@fiws122.cs.tu-dortmund.de>
2018-12-04 16:17:56 +0100

    Add config-file Support in Sample-Creation-Script

--- createsamplesheet.py
+++ createsamplesheet.py
@@ -1,22 +1,37 @@
 import os
 import pandas as pd
+import yaml
 
-#Get current dictonary
+#Read Yamlfile
+stream = open("sampleconfig.yaml", "r")
+doc = yaml.load(stream)
+
+
+#Get current directory
 cwd = os.getcwd()
 
-#Test dictonary
-cwd += "/Tests/"
 
-#Sampledict
+#Get Sampledict
+#If Testing is active add Test directory
+if doc['testing']:
+	cwd += "/Tests/"
 cwd += "reads/"
-files = [f for f in os.listdir(cwd) if os.path.isfile(os.path.join(cwd, f))]
-files.sort()
 
-#Create Dictonary for DataFrame
-sdict = {"sample" : ["a","b"], "fq1" : [files[0],files[2]], "fq2" : [files[1],files[3]], "condition" : ["treated", "untreated"]}
-df = pd.DataFrame(sdict)
+
+#Import samples as stated in Configfile
+samples = doc['samples']
+sample_dict = {"sample" : [], "fq1" : [], "fq2" : [], "condition" : []}
+for key in samples:
+	if len(samples[key]) != 3:
+		raise IndexError("False number of Values! Each Sample needs two reads and a condition.")
+	#Import Values into Dictonary
+	sample_dict["sample"].append(key)
+	sample_dict["fq1"].append(samples[key][0])
+	sample_dict["fq2"].append(samples[key][1])
+	sample_dict["condition"].append(samples[key][2])
+
+#Create DataFrame from Dictonary, set Samplename as index and save it as .tsv file
+df = pd.DataFrame(sample_dict)
 df.set_index("sample")
 df.to_csv("samplesheet.tsv", sep='\t', encoding = 'utf-8', index = False)
-
-
 
