326aa68ff273fa8193e37e4c6c05279132cedd24
OleDickel <ole.dickel@gmx.de>
2019-01-10 15:50:11 +0100

    sachen von Hatim gefixt

---
+++ .snakemake/conda/7bf80ada/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/lib"
+XML2_LIBS="-lxml2 -L/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/lib -lz -L/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/lib -llzma  -L/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/lib -liconv -L/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/include/libxml2 -I/home/dickel00/FP/Bioinfo/FPBioinformatik/.snakemake/conda/7bf80ada/include"
+MODULE_VERSION="xml2-2.9.8"
+
