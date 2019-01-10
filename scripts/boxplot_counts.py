adf2ba1ffd57562e328e910148f4580dc5c80159
Hatim Bouzian <hatimbouzian@live.de>
2019-01-10 16:18:18 +0100

    Delete .snakemake

--- scripts/boxplot_counts.py
+++ scripts/boxplot_counts.py
@@ -1,3 +1,14 @@
 import seaborn as sns
+import pandas as pd
+import yaml
+
+with open("../config.yaml", 'r') as yamlfile:
+    config = yaml.load(yamlfile)
+
+testdr = ""
+if config["testing"]:
+	testdr = "Tests/"
 
 
+samples = pd.read_table("../sleuthResults/normCounts.tsv")
+for sample in samples.c
