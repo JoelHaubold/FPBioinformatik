4713caa645fc59ebcdbea6dc76b8745d9514e6fe
Hatim Bouzian <bouzia00@fiws155.cs.tu-dortmund.de>
2019-01-24 17:31:26 +0100

    Edit PCA-Plot Script

--- scripts/pca_plot.py
+++ scripts/pca_plot.py
@@ -1,5 +1,4 @@
 import os
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
@@ -17,7 +16,36 @@ for index, row in sample_sheet.iterrows():
 	conditions[row['sample']] = row['condition']
 
 
-x = samples.columns.values[1:]
-print(x) 
+features = list(samples.columns.values[1:])
+
+y = []
+for sample in features:
+	y.append(conditions[sample])
+target = []
+for condition in y:
+	if condition not in target:
+		target.append(condition)
+
+x = samples.loc[:, features].values
+
+pca = PCA()
+
+x_pca = pca.fit_transform(x)
+
+pca_df = pd.DataFrame(data = x_pca, columns = features)
+pca_df = pca_df.transpose()
+pca_df['Condition'] = y
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('X-Axis', fontsize = 15)
+ax.set_ylabel('Y-Axis', fontsize = 15)
+colors = ['r','b']
+
+#for targ , color in zip(target, colors):
+	
+
+print(pca_df)
+
 print("\n")
-print(conditions)
+#print(conditions)
