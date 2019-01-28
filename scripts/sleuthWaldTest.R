eeb237e0b290cb328a1d9e73d6a6149247e69f28
OleDickel <ole.dickel@gmx.de>
2019-01-28 15:53:49 +0100

    waldtest, snakefile scriptstuff

--- scripts/sleuthWaldTest.R
+++ scripts/sleuthWaldTest.R
@@ -20,7 +20,7 @@ sleuth_significant <- dplyr::filter(sleuth_table, qval <= 0.05)
 print(head(sleuth_significant, 20))
 
 write.table(sleuth_table, file=snakemake@output[1], quote=FALSE, sep='\t', col.names = NA)
-
+sleuth_save(so, file = snakemake@output[2])
 
 # plot_bootstrap(so, "ENST00000291572", units = "est_counts", color_by = "condition")
 # plot_bootstrap(so, "ENST00000474114", units = "est_counts", color_by = "condition")
