85b568de2289351a3a46e721be704012f18ece8e
Hatim Bouzian <hatimbouzian@live.de>
2019-01-28 14:39:04 +0100

    Delete strip

--- scripts/pca_plot.py
+++ scripts/pca_plot.py
@@ -13,18 +13,17 @@ sample_sheet = pd.read_table(path_sheet)
 # Make Dictonary with Samplename as key and Condition as value
 conditions = {}
 for index, row in sample_sheet.iterrows():
-	conditions[row['sample']] = row['condition']
-
+    conditions[row['sample']] = row['condition']
 
 features = list(samples.columns.values[1:])
 
 y = []
 for sample in features:
-	y.append(conditions[sample])
+    y.append(conditions[sample])
 target = []
 for condition in y:
-	if condition not in target:
-		target.append(condition)
+    if condition not in target:
+        target.append(condition)
 
 x = samples.loc[:, features].values
 
@@ -32,20 +31,20 @@ pca = PCA()
 
 x_pca = pca.fit_transform(x)
 
-pca_df = pd.DataFrame(data = x_pca, columns = features)
+pca_df = pd.DataFrame(data=x_pca, columns=features)
 pca_df = pca_df.transpose()
 pca_df['Condition'] = y
 
-fig = plt.figure(figsize = (8,8))
-ax = fig.add_subplot(1,1,1)
-ax.set_xlabel('X-Axis', fontsize = 15)
-ax.set_ylabel('Y-Axis', fontsize = 15)
-colors = ['r','b']
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(1, 1, 1)
+ax.set_xlabel('X-Axis', fontsize=15)
+ax.set_ylabel('Y-Axis', fontsize=15)
+colors = ['r', 'b']
 
-#for targ , color in zip(target, colors):
-	
+# for targ , color in zip(target, colors):
+
 
 print(pca_df)
 
 print("\n")
-#print(conditions)
+# print(conditions)
